9a56b203e2f72b4a3061040fc80f608aad947706
weldcell <weldcell@ipa.fraunhofer.de>
2022-01-19 15:03:44 +0100

    fix for cmake install

--- micro_epsilon_scancontrol_driver/setup.py
+++ micro_epsilon_scancontrol_driver/setup.py
@@ -3,10 +3,14 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
-    packages=['micro_epsilon_scancontrol_driver', 'micro_epsilon_scancontrol_driver.plugins'],
-    package_dir={'': 'src'},
-    scripts=['scripts/me_scancontrol_plugins']
-)
+# d = generate_distutils_setup(
+#     packages=['micro_epsilon_scancontrol_driver', 'micro_epsilon_scancontrol_driver.plugins'],
+#     package_dir={'': 'src'},
+#     scripts=['scripts/me_scancontrol_plugins']
+# )
+
+# setup(**d)
+
+d = generate_distutils_setup()
 
 setup(**d)
